2bccc3a4860e78f348a50221d6557d7b29786848
PavelPikarin <ppikarin@gmail.com>
2022-03-17 19:28:28 +0500

    change

--- src/main/java/com/ppikarin/auto1/Main.java
+++ src/main/java/com/ppikarin/auto1/Main.java
@@ -3,7 +3,8 @@ package com.ppikarin.auto1;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+
+        System.out.println("hello");
     }
 
 }
