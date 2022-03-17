9dcbcfb489f31cb9f34c7d7fc63c28548a51ff77
PavelPikarin <ppikarin@gmail.com>
2022-03-17 18:33:28 +0500

    init

---
+++ src/test/java/com/ppikarin/auto1/MainTest.java
@@ -0,0 +1,16 @@
+package com.ppikarin.auto1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+//@SpringBootTest
+public class MainTest {
+
+    @Test
+    public void test() throws Exception {
+
+        assertTrue(true);
+    }
+}
